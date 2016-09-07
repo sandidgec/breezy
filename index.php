28263dcf2a6fcc430a5a9a58cc031b3cf2fda00b
Charles Sandidge <sandidgec@gmail.com>
2016-09-07 08:48:59 -0600

    breezySite
    new site for J. J.

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Breezy Consulting</title>
+    <?php require_once("lib/head-utils.php");?>
+  </head>
+  <body>
+      <div class="jumbotron">
+        <div class="col-md-8 col-md-offset-2">
+          <h3 class="text-center juliatext">Julia Jones</h>
+
+          <div class="row col-md-10 col-md-offset-1">
+            <div class="col-md-3 round">
+                <h4 id="textCir">ABOUT</h>
+            </div>
+
+            <img src="lib/images/juliaPic.jpg" alt="JuliaPic"
+           class="col-md-4 col-md-offset-1 img-responsive img-circle center-block"/>
+
+           <div class="col-md-3 round">
+               <h4 id="textCir">SERVICES</h>
+           </div>
+
+         </div>
+            </div>
+                <div class="col-md-12 text-center">
+                    <i class="fa fa-facebook" aria-hidden="true"></i>
+                    <i class="fa fa-envelope" aria-hidden="true"></i>
+                    <i class="fa fa-twitter" aria-hidden="true"></i>
+                </div>
+            </div>
+</div>
+
+
+  </body>
+</html>
