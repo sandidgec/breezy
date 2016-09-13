83596eb2cb623e2e5f753092eaf2c72e87d2170a
Krieg Benally <kriegbenally@gmail.com>
2016-09-12 21:44:26 -0600

    added modals
    I would like some content to place in these modals.

--- index.php
+++ index.php
@@ -25,13 +25,18 @@
       <div class="container">
         <div class="row centerAlign">
           <div class=" centerAlign col-md-4">
-            <a href="#"><h3>about</h3></a>
-          </div>
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#aboutModal">
+              about
+            </button>          </div>
           <div class="col-md-4 centerAlign">
             <img class="img-responsive img-circle" style="margin:25px;"src="lib/images/juliaPic.jpg" alt="" />
           </div>
           <div class=" centerAlign col-md-4">
-            <a href="#"><h3>services</h3></a>
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#servicesModal">
+              services
+            </button>
           </div>
         </div>
 
@@ -46,11 +51,49 @@
             <i class="fa fa-twitter" aria-hidden="true"></i>
           </div>
         </div>
-
       </div>
+    </div>
 
 
 
+    <!-- aboutModal -->
+    <div class="modal fade" id="aboutModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" id="myModalLabel">about</h4>
+          </div>
+          <div class="modal-body">
+            ...
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="servicesModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" id="myModalLabel">services</h4>
+          </div>
+          <div class="modal-body">
+            ...
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
+          </div>
+        </div>
+      </div>
     </div>
 
 
